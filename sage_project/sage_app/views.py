06135d2949bf007dd949fe069ec64fbf6cb6a877
Chibu <atuchibueze@gmail.com>
2022-11-02 22:05:05 -0400

    implement login

--- sage_project/sage_app/views.py
+++ sage_project/sage_app/views.py
@@ -1,17 +1,40 @@
-from mimetypes import init
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.db.models import Q
-from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
-rooms = [
-  {'id': 1, 'name': "Let's learn python"},
-  {'id': 2, 'name': "Design with me"},
-  {'id': 3, 'name': "Frontend developers"},
-]
+# rooms = [
+#   {'id': 1, 'name': "Let's learn python"},
+#   {'id': 2, 'name': "Design with me"},
+#   {'id': 3, 'name': "Frontend developers"},
+# ]
+
+def loginPage(request):
+
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    try:
+      user = User.objects.get(username=username)
+    except:
+      messages.error(request, 'User does not exist')
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is not None:
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'Username or password is incorrect')
+
+  context = {}
+  return render(request, 'sage_app/login_register.html', context)
 
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
