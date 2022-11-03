83b24157fe636884987006a2bb2cf12edec62640
Chibu <atuchibueze@gmail.com>
2022-11-03 03:27:46 -0400

    implement user registration

--- sage_project/sage_app/views.py
+++ sage_project/sage_app/views.py
@@ -1,8 +1,10 @@
+import re
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.db.models import Q
 from .models import Room, Topic
@@ -17,12 +19,13 @@ from .forms import RoomForm
 # ]
 
 def loginPage(request):
+  page = 'login'
 
   if request.user.is_authenticated:
     return redirect('home')
 
   if request.method == 'POST':
-    username = request.POST.get('username')
+    username = request.POST.get('username').lower()
     password = request.POST.get('password')
 
     try:
@@ -38,12 +41,28 @@ def loginPage(request):
     else:
       messages.error(request, 'Username or password is incorrect')
 
-  context = {}
+  context = {'page': page}
   return render(request, 'sage_app/login_register.html', context)
 
 def logoutUser(request):
   logout(request)
   return redirect('home')
+
+def registerPage(request):
+  form = UserCreationForm()
+
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False)
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'Error occurred during registration')
+
+  return render(request, 'sage_app/login_register.html', {'form': form})
 
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
